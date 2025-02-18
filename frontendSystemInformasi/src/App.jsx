c8a542a3a609d16893e5ac8af390674d2ed360d4
Daffa Halim <daffahalim187@gmail.com>
2025-02-18 07:08:30 +0700

    Memperbaiki route

--- frontendSystemInformasi/src/App.jsx
+++ frontendSystemInformasi/src/App.jsx
@@ -15,6 +15,18 @@ import DataKurikulum from './pages/DataKurikulum';
 import AddKurikulum from './pages/AddKurikulum';
 
 
+
+//Import
+import Homepage from "./view/Homepage";
+import Jurusan from './view/Jurusan.jsx';
+import Blogpage from './view/Blogpage.jsx';
+import Loginpage from './view/Loginpage.jsx';
+import Registerpage from './view/Registerpage.jsx';
+import Kegiatan from './view/Kegiatan.jsx';
+import Targetjurusan from './view/Targetjurusan.jsx';
+
+
+
 function App() {
  
   return(
@@ -29,13 +41,25 @@ function App() {
       <Route path='/add_kegiatan' element={<AddKegiatan />} />
       
       {/* Jurusan */}
-      <Route path='/data_jurusan' element={<DataJurusan />} />
+      <Route path='/detail_jurusan' element={<DataJurusan />} />
       <Route path='/add_jurusan' element={<AddJurusan />} />
       
       {/* Kurikulum */}
       <Route path='/data_kurikulum' element={<DataKurikulum />} />
       <Route path='/add_kurikulum' element={<AddKurikulum />} />
     
+
+
+      {/* Gibran */}
+
+      <Route path="/" element={<Homepage />} />
+    {/* <Route path="Crud" element={<Crud />} /> */}
+    <Route path="Blog" element={<Blogpage />} />
+    <Route path="Jurusan" element={<Jurusan />} />
+    <Route path="Login" element={<Loginpage />} />
+    <Route path="Register" element={<Registerpage />} />
+    <Route path="Kegiatan" element={<Kegiatan />} />
+    <Route path="Targetjurusan" element={<Targetjurusan />} />
       </Routes>
     </BrowserRouter>
   )
