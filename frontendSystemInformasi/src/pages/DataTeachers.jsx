f23f23991259f32a7ae04b10f1d45006d01462c2
Patriott07 <pwangtampn@gmail.com>
2025-02-27 22:44:47 +0700

    ketinggal

--- frontendSystemInformasi/src/pages/DataTeachers.jsx
+++ frontendSystemInformasi/src/pages/DataTeachers.jsx
@@ -223,7 +223,7 @@ const DataTeachers = () => {
                         </div>
                         <Link
                             to={`/add_teacher/${id}`}
-                            className="w-fit mt-3 mb-4 text-white bg-purple-500 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+                            className="w-fit mt-3 mb-4 text-white bg-purple-500 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2"
                         >
                             Tambah Data
                         </Link>
